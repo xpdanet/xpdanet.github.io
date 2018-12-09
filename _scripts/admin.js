6b58e7db1882e0c422435611d844a59f6720b652
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2018-12-08 21:58:55 -0500

    Split out data, Admin Panel (#5)
    * split out tools
    * Created Admin page
    * beautify json
    * display all existing data
    * minus.vue

---
+++ _scripts/admin.js
@@ -0,0 +1,57 @@
+// eslint-disable-next-line no-unused-vars
+let app = new Vue({
+  el: '#app',
+  components: {
+    minus: httpVueLoader('_components/minus.vue')
+  },
+  data: {
+    selectedTool: 'new',
+    tools: null,
+    tool: null
+  },
+  methods: {
+    clearForm: function () {
+      this.tool = {
+        title: '',
+        url: '',
+        logo: '',
+        deprecated: false,
+        languages: [],
+        platforms: [],
+        pros: [],
+        cons: [],
+        frameworks: [],
+        tools: [],
+        tutorials: [],
+        notableProjects: []
+      };
+    },
+    addNew: function (section, type) {
+      type = type || 'text';
+      if (type === 'text') {
+        this.tool[section].push('');
+      }
+    }
+  },
+  filters: {
+    JSONstringify: function (val) {
+      return beautifyJSON(val);
+    }
+  },
+  watch: {
+    selectedTool: function (title) {
+      if (title === 'new') {
+        this.clearForm();
+      } else {
+        this.tools.forEach((tool) => {
+          if (tool.title === title) {
+            this.tool = tool;
+          }
+        });
+      }
+    },
+    tools: function () {
+      this.clearForm();
+    }
+  }
+});
