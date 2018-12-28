19db99202960574cbec5c95405f6c355ca3e1a37
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2018-12-28 09:47:09 -0500

    Chromely (#13)
    * Chromely
    * url params on admin

--- _scripts/admin.js
+++ _scripts/admin.js
@@ -21,14 +21,46 @@ let app = new Vue({
     getToolsData: function () {
       helpers.getToolsData()
         .then((response) => {
-          let tools = response.tools;
-          this.tools = tools;
-          this.platforms = helpers.setFilterValues(tools, 'platforms');
-          this.languages = helpers.setFilterValues(tools, 'languages');
-          this.sites = helpers.listAllSites(tools);
-          this.frameworks = helpers.listAllFrameworks(tools);
+          this.tools = response.tools;
           this.networkError = response.networkError;
         });
+    },
+    parseSelectedToolFromUrl: function () {
+      // ?tool=Adobe%20Animate&x=true
+      let params = location.search
+        // remove ?
+        .slice(1)
+        // [ 'tool=Adobe%20Animate', 'x=true' ]
+        .split('&')
+        // [ ['tool', 'Adobe%20Animate'], ['x', 'true'] ]
+        .map((param) => {
+          return param.split('=');
+        })
+        // { tool: 'Adobe Animate', x: 'true' }
+        .reduce((obj, [key, value]) => {
+          return {
+            ...obj,
+            [key]: decodeURI(value)
+          };
+        }, {});
+
+      if (params.tool) {
+        this.selectedTool = params.tool;
+      }
+    },
+    setToolInUrl: function (title) {
+      if (history.pushState) {
+        let newUrl = [
+          location.protocol,
+          '//',
+          location.host,
+          location.pathname,
+          '?tool=',
+          encodeURI(title)
+        ].join('');
+
+        history.pushState({ path: newUrl }, '', newUrl);
+      }
     },
     clearForm: function () {
       this.tool = {
@@ -109,10 +141,16 @@ let app = new Vue({
             this.tool = tool;
           }
         });
+        this.setToolInUrl(title);
       }
     },
-    tools: function () {
+    tools: function (tools) {
+      this.platforms = helpers.setFilterValues(tools, 'platforms');
+      this.languages = helpers.setFilterValues(tools, 'languages');
+      this.sites = helpers.listAllSites(tools);
+      this.frameworks = helpers.listAllFrameworks(tools);
       this.clearForm();
+      this.parseSelectedToolFromUrl();
     }
   },
   created: function () {
