b8d6cc7130a5e9940e70caecbe49a9dfe91e957b
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-04-07 18:39:06 -0400

    Write test to validate JSON file

---
+++ test.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const helpers = require('./_scripts/helpers.js');
+
+const tools = String(fs.readFileSync('./_data/tools.json'));
+const parsed = helpers.parseToolsData(tools);
+
+if (parsed && parsed.tools && parsed.tools.length && Array.isArray(parsed.tools)) {
+  console.log('tools.json was validated');
+} else {
+  throw 'tools.json is not valid';
+}
