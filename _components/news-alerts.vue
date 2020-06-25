fd85b2b51ff64f3c99c81ee5db4ada7c45f5fb74
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-06-25 10:27:00 -0400

    Update news-alerts.vue (#73)

--- _components/news-alerts.vue
+++ _components/news-alerts.vue
@@ -35,7 +35,7 @@ module.exports = {
         {
           date: '2019-11-09T00:02:00.000Z',
           headline: 'Apple blocking Electron',
-          summary: '(UPDATED) Electron 8.0.2 and 9.0.0-beta3 was released on 2020-02-25, and apps report being let through again. Previously, dozens of Electron apps reported being blocked by Apple's App Store due to the Electron framework using private APIs, with mixed reports of Apple letting some apps through anyway.',
+          summary: '(UPDATED) Electron 8.0.2 and 9.0.0-beta3 was released on 2020-02-25, and apps report being let through again. Previously, dozens of Electron apps reported being blocked by Apple\'s App Store due to the Electron framework using private APIs, with mixed reports of Apple letting some apps through anyway.',
           url: 'https://github.com/electron/electron/issues/20027#issuecomment-591458648'
         }
       ]
