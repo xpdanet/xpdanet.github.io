48ad345e297c15e87d5995473646e48b56a54d13
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-08-28 11:11:39 -0400

    Update _components/news-alerts.vue

--- _components/news-alerts.vue
+++ _components/news-alerts.vue
@@ -23,7 +23,7 @@ module.exports = {
         {
           data: '2020-08-26T19:39:17.000Z',
           headline: 'Electron drops ESM support',
-          summary: 'Electron has stated that it will be dropping support for JavaScript\'s modern native import/export. The popular <code>import module from \'./module.js\'</code> pattern will be removed from Electron.',
+          summary: 'Electron has stated that it will be dropping support for JavaScript\'s modern, native, import/export. The popular <code>import module from \'./module.js\'</code> pattern will be removed from Electron.',
           url: 'https://github.com/electron/electron/issues/21457#issuecomment-647728163'
         },
         {
